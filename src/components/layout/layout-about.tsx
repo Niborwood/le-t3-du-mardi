cf5192ebb91903975473134f2f9f1ec2dd779748
Robin Souriau <robin.souriau@gmail.com>
2022-11-23 23:48:34 +0100

    Place item center

--- src/components/layout/layout-about.tsx
+++ src/components/layout/layout-about.tsx
@@ -1,6 +1,6 @@
 const LayoutAbout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <footer className="order-last h-full w-full 2xl:order-none">
+    <footer className="order-last grid h-full w-full place-items-center 2xl:order-none">
       {children}
     </footer>
   );
