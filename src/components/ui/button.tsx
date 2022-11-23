69e43fee7052a9da4d3ea001c6da99db7c27d26d
Robin SOURIAU <robin.souriau@gmail.com>
2022-11-24 00:21:31 +0100

    Button UI, lighter green for app

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from "react";
+import classNames from "classnames";
 
 type ButtonProps = {
   children: ReactNode;
@@ -6,8 +7,28 @@ type ButtonProps = {
   disabled?: boolean;
 };
 
-const Button = ({ children, variant = "primary", disabled }: ButtonProps) => {
-  return <button type="button">{children}</button>;
+const Button = ({
+  children,
+  variant = "primary",
+  disabled = false,
+}: ButtonProps) => {
+  return (
+    <button
+      type="button"
+      className={classNames(
+        "rounded-lg p-6 font-clash text-4xl font-semibold uppercase transition-all",
+        {
+          "cursor-not-allowed opacity-50 hover:text-zinc-50": disabled,
+          "bg-emerald-600 text-zinc-50 hover:bg-emerald-700 hover:text-zinc-50":
+            variant === "primary",
+          "border-4 border-zinc-900 bg-transparent text-zinc-900 hover:border-emerald-600 hover:text-emerald-600":
+            variant !== "primary",
+        }
+      )}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
