199189a1957c18ac080e12c424b3f4344ef7399e
Robin Souriau <robin.souriau@gmail.com>
2022-11-24 21:46:03 +0100

    Better alignment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,23 +52,25 @@ const Home: NextPage = () => {
 
       {/* PREVIOUS */}
       <LayoutPrev>
-        <main className="space-between order-2 flex flex-col gap-2 rounded-md  bg-zinc-50 p-2 2xl:order-none 2xl:grid-rows-4 2xl:gap-4">
-          {lastTopics.data?.map((topic, index) => (
-            <button
-              className={`flex flex-row-reverse items-center justify-end gap-2 rounded-md bg-zinc-50 px-4 py-6 text-left text-xl font-light uppercase text-zinc-900 transition-all hover:scale-105 lg:py-3 2xl:gap-4 2xl:py-0 2xl:row-start-${
-                index + 2
-              }`}
-              key={topic.id}
-            >
-              <span>
-                Top 3{" "}
-                <span className="font-semibold text-emerald-700">
-                  {topic.name}
+        <main className="order-2 flex flex-col justify-between gap-2 rounded-md  bg-zinc-50 p-2 2xl:order-none 2xl:grid-rows-4 2xl:gap-4">
+          <div className="space-y-4">
+            {lastTopics.data?.map((topic, index) => (
+              <button
+                className={`flex flex-row-reverse items-center justify-end gap-2 rounded-md bg-zinc-50 px-4 py-6 text-left text-xl font-light uppercase text-zinc-900 transition-all hover:scale-105 lg:py-3 2xl:gap-4 2xl:py-0 2xl:row-start-${
+                  index + 2
+                }`}
+                key={topic.id}
+              >
+                <span>
+                  Top 3{" "}
+                  <span className="font-semibold text-emerald-700">
+                    {topic.name}
+                  </span>
                 </span>
-              </span>
-              <ArrowRight className="min-w-[30px] 2xl:-rotate-45" />
-            </button>
-          ))}
+                <ArrowRight className="min-w-[30px] 2xl:-rotate-45" />
+              </button>
+            ))}
+          </div>
 
           {/* See More */}
           <Button variant="secondary" size="md">
