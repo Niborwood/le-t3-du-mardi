0a93bf10d414fc3246c2873c701ac34bf0fdd19e
Robin SOURIAU <robin.souriau@gmail.com>
2022-11-26 16:47:24 +0100

    About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,27 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const About: NextPage = () => {
-  return <div>About</div>;
+  return (
+    <div className="font-mono text-xs">
+      <p>&gt;&gt;&gt; Finite Inc</p>
+      <p>
+        <a
+          href="https://github.com/Niborwood"
+          target="_blank"
+          rel="noreferrer"
+          className="underline"
+        >
+          @RobinSouriau
+        </a>
+      </p>
+      <br />
+      <br />
+      <p>
+        <Link href="/">&lt;</Link>
+      </p>
+    </div>
+  );
 };
 
 export default About;
