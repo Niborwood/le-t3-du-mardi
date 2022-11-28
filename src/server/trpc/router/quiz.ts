c3942df25ed2426569ed9ddf22684051dd3272fc
Robin Souriau <robin@diggersfactory.com>
2022-11-28 16:39:45 +0100

    Delete hello from quiz routes

--- src/server/trpc/router/quiz.ts
+++ src/server/trpc/router/quiz.ts
@@ -4,14 +4,6 @@ import { z } from "zod";
 import { router, publicProcedure, protectedProcedure } from "../trpc";
 
 export const quizRouter = router({
-  hello: publicProcedure
-    .input(z.object({ text: z.string().nullish() }).nullish())
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
-    }),
-
   // Query procedures
   getAllTopics: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.topic.findMany();
