10ef7b9f73c606ff459fd0831e7333d19e868516
Robin Souriau <robin.souriau@gmail.com>
2022-12-17 20:01:45 +0100

    isActive menu mobile

--- src/components/layout/menu-dialog.tsx
+++ src/components/layout/menu-dialog.tsx
@@ -1,6 +1,8 @@
 import { Dialog } from "@headlessui/react";
+import classNames from "classnames";
 import { ArrowUpRight, Plus } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import type { Dispatch, SetStateAction } from "react";
 import type { menus } from "./layout-menu";
 
@@ -11,23 +13,46 @@ type MenuDialogProps = {
 };
 
 export const MenuDialog = ({ isOpen, setIsOpen, items }: MenuDialogProps) => {
+  const router = useRouter();
+
   return (
     <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
       <section className="fixed inset-0 z-50 grid place-items-center">
         <Dialog.Panel className="grid h-screen w-screen place-items-center bg-emerald-600 p-8 font-clash text-5xl font-bold uppercase text-zinc-50">
           <section className="flex flex-col gap-8">
-            {items.map((item) => (
-              <Link
-                className="group"
-                key={item.id}
-                href={`/${item.id === "home" ? "" : item.id}`}
-              >
-                <span className="transition-all group-hover:underline">
-                  {item.name}
-                </span>{" "}
-                <ArrowUpRight className="transition-all group-hover:rotate-45" />
-              </Link>
-            ))}
+            {items.map((item) => {
+              const isActive =
+                router.pathname === "/"
+                  ? item.id === "home"
+                  : router.pathname.includes(item.id);
+
+              return (
+                <Link
+                  className="group"
+                  key={item.id}
+                  href={`/${item.id === "home" ? "" : item.id}`}
+                >
+                  <span
+                    className={classNames(
+                      "transition-all group-hover:underline",
+                      {
+                        underline: isActive,
+                      }
+                    )}
+                  >
+                    {item.name}
+                  </span>{" "}
+                  <ArrowUpRight
+                    className={classNames(
+                      "transition-all group-hover:rotate-45",
+                      {
+                        "-rotate-45": isActive,
+                      }
+                    )}
+                  />
+                </Link>
+              );
+            })}
           </section>
         </Dialog.Panel>
         <button
