8b88c0ae9ec1d0e61a73106a8343299080936f20
Robin Souriau <robin@diggersfactory.com>
2022-12-19 14:17:34 +0100

    Testing OG and default text

--- src/components/layout/global-layout.tsx
+++ src/components/layout/global-layout.tsx
@@ -12,6 +12,10 @@ const GlobalLayout = ({ children }: { children: React.ReactNode }) => {
           name="description"
           content="Tous les jours, un top 3 de n'importe quoi."
         />
+        <meta
+          property="og:image"
+          content="https://top3dumardi.vercel.app/api/og?top=n%27importe%20quoi"
+        />
       </Head>
       <main className="mt-10 grid grid-rows-1 gap-4 p-2 sm:min-h-screen lg:mt-0 lg:max-h-screen lg:grid-cols-3 lg:grid-rows-3 lg:p-4 2xl:grid-rows-3">
         {children}
---
+++ src/pages/api/og.tsx
@@ -0,0 +1,71 @@
+import { ImageResponse } from "@vercel/og";
+import type { NextRequest } from "next/server";
+
+export const config = {
+  runtime: "experimental-edge",
+};
+
+const font = fetch(
+  new URL("../../assets/ClashDisplay-Bold.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+
+const og = async function (req: NextRequest) {
+  const fontData = await font;
+  const { searchParams } = req.nextUrl;
+  const top = searchParams.get("top");
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#059669",
+        }}
+      >
+        <div tw="bg-emerald-600 flex w-full h-full items-center justify-center">
+          <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between">
+            <h2 tw="flex flex-col text-3xl sm:text-4xl tracking-tight text-zinc-50 text-left">
+              <span tw="mb-4">Aujourd&apos;hui, c&apos;est top 3.</span>
+              <span tw="text-9xl mb-12">Top 3 {top}</span>
+            </h2>
+            {/* <div tw="rounded-lg border border-4 p-4 border-zinc-50 flex text-3xl uppercase items-center">
+              <span tw="mr-8 text-zinc-50">Voter</span>{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="60"
+                height="60"
+                viewBox="0 0 24 24"
+                fill="transparent"
+                stroke="white"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+                <polyline points="12 5 19 12 12 19"></polyline>
+              </svg>
+            </div> */}
+          </div>
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "Clash",
+          data: fontData,
+          style: "normal",
+        },
+      ],
+    }
+  );
+};
+
+export default og;
