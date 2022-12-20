54a3ffe2e9f5b1f54266fa4c9bc1d2022a5b5370
Robin SOURIAU <robin.souriau@gmail.com>
2022-12-20 22:13:22 +0100

    Remove comments on og

--- src/components/ui/fullscreenLoader.tsx
+++ src/components/ui/fullscreenLoader.tsx
@@ -1,11 +1,8 @@
 import Head from "next/head";
 import React from "react";
 import { weekday } from "../../utils";
-import { trpc } from "../../utils/trpc";
 
 export const FullscreenLoader = () => {
-  const { data: currentTopic } = trpc.quiz.getCurrentTopic.useQuery();
-
   return (
     <div className="grid h-screen w-screen place-items-center bg-emerald-600 p-4 font-clash text-6xl font-bold text-zinc-50 lg:text-8xl 2xl:text-9xl">
       <Head>
--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -34,23 +34,6 @@ const og = async function (req: NextRequest) {
               <span tw="mb-4">Aujourd&apos;hui, c&apos;est top 3.</span>
               <span tw="text-9xl mb-12">Top 3 {top}.</span>
             </h2>
-            {/* <div tw="rounded-lg border border-4 p-4 border-zinc-50 flex text-3xl uppercase items-center">
-              <span tw="mr-8 text-zinc-50">Voter</span>{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="60"
-                height="60"
-                viewBox="0 0 24 24"
-                fill="transparent"
-                stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <line x1="5" y1="12" x2="19" y2="12"></line>
-                <polyline points="12 5 19 12 12 19"></polyline>
-              </svg>
-            </div> */}
           </div>
         </div>
       </div>
