0cb532d96d01c33bfb90d3ca3238c309e8d082ca
Robin SOURIAU <robin.souriau@gmail.com>
2022-12-20 21:35:40 +0100

    Testing og name

--- src/components/layout/global-layout.tsx
+++ src/components/layout/global-layout.tsx
@@ -1,11 +1,15 @@
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import { useEffect, useState } from "react";
+import { trpc } from "../../utils/trpc";
 import { FullscreenLoader } from "../ui";
 
 const GlobalLayout = ({ children }: { children: React.ReactNode }) => {
   const { status } = useSession();
   const [secondLoaded, setSecondLoaded] = useState(false);
+
+  const { data: currentTopic } = trpc.quiz.getCurrentTopic.useQuery();
+  console.log(currentTopic);
 
   // If session is loading, show loading screen for at least 1 second
   useEffect(() => {
@@ -29,7 +33,7 @@ const GlobalLayout = ({ children }: { children: React.ReactNode }) => {
         />
         <meta
           property="og:image"
-          content="https://top3dumardi.vercel.app/api/og?top=n%27importe%20quoi"
+          content={`https://top3dumardi.vercel.app/api/og?top=${currentTopic?.name}}`}
         />
       </Head>
       <main className="mt-10 grid grid-rows-1 gap-4 p-2 sm:min-h-screen lg:mt-0 lg:max-h-screen lg:grid-cols-3 lg:grid-rows-3 lg:p-4 2xl:grid-rows-3">
