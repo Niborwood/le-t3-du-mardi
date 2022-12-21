392a80f3f38f0390c7212915b39b375098ff5732
Robin SOURIAU <robin.souriau@gmail.com>
2022-12-21 01:22:01 +0100

    cant use prisma on og, revert to url param

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -1,6 +1,5 @@
 import { ImageResponse } from "@vercel/og";
 import type { NextRequest } from "next/server";
-import { prisma } from "../../server/db/client";
 
 export const config = {
   runtime: "experimental-edge",
@@ -14,12 +13,6 @@ const og = async function (req: NextRequest) {
   const fontData = await font;
   const { searchParams } = req.nextUrl;
   const top = searchParams.get("top");
-
-  const currentTopic = await prisma.topic.findFirst({
-    where: {
-      current: true,
-    },
-  });
 
   return new ImageResponse(
     (
@@ -38,9 +31,7 @@ const og = async function (req: NextRequest) {
           <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between">
             <h2 tw="flex flex-col text-3xl sm:text-4xl tracking-tight text-zinc-50 text-left">
               <span tw="mb-4">Aujourd&apos;hui, c&apos;est top 3.</span>
-              <span tw="text-9xl mb-12">
-                Top 3 {currentTopic?.name || "n'importe quoi"}.
-              </span>
+              <span tw="text-9xl mb-12">Top 3 {top || "n'importe quoi"}.</span>
             </h2>
           </div>
         </div>
