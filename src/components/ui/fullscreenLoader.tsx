05c16ececbb0ac1d860aeafad7f7fcd082f90642
Robin SOURIAU <robin.souriau@gmail.com>
2022-12-21 01:26:58 +0100

    Add Title to Loading Screen

--- src/components/ui/fullscreenLoader.tsx
+++ src/components/ui/fullscreenLoader.tsx
@@ -6,6 +6,7 @@ export const FullscreenLoader = () => {
   return (
     <div className="grid h-screen w-screen place-items-center bg-emerald-600 p-4 font-clash text-6xl font-bold text-zinc-50 lg:text-8xl 2xl:text-9xl">
       <Head>
+        <title>Le Top 3 du {weekday}</title>
         <meta
           name="description"
           content="Tous les jours, un top 3 de n'importe quoi."
