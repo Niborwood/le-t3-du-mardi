bfb93c400b17909e1850ccdadbfd99d636206cbb
Robin Souriau <robin@diggersfactory.com>
2022-12-23 16:41:23 +0100

    Revert fullscreen

--- src/components/layout/global-layout.tsx
+++ src/components/layout/global-layout.tsx
@@ -1,6 +1,24 @@
+import { useSession } from "next-auth/react";
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import { FullscreenLoader } from "../ui";
 
 const GlobalLayout = ({ children }: { children: React.ReactNode }) => {
+  const { status } = useSession();
+  const [timeLoaded, setTimeLoaded] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setTimeLoaded(true);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, [status]);
+
+  if (status === "loading" || !timeLoaded) {
+    return <FullscreenLoader />;
+  }
+
   return (
     <div className="min-h-screen animate-fade-in bg-zinc-900 bg-gradient-to-tr from-zinc-900 to-zinc-800/70 font-archivo font-extralight text-zinc-50">
       <Head>
