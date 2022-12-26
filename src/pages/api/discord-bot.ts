c3ccf38fa5a4c5d0871914c3785a091b2cbf5a66
Robin Souriau <robin.souriau@gmail.com>
2022-12-26 12:31:22 +0100

    Add username change on discord bot

--- src/pages/api/discord-bot.ts
+++ src/pages/api/discord-bot.ts
@@ -1,16 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { env } from "../../env/server.mjs";
+import { weekday } from "../../utils/";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { authorization } = req.headers;
-    if (!authorization || authorization !== `Bearer ${process.env.API_SECRET}`)
+    if (!authorization || authorization !== `Bearer ${env.API_SECRET}`)
       return res.status(401).json({ success: false });
 
     await fetch(env.DISCORD_WEBHOOK, {
       method: "POST",
       body: JSON.stringify({
         content: `Aujourd'hui c'est top 3 ! \n \n https://top3dumardi.vercel.app/`,
+        username: `Le Top 3 du ${
+          weekday.charAt(0).toUpperCase() + weekday.slice(1)
+        }`,
       }),
       headers: {
         "Content-Type": "application/json",
